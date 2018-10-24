bf3b26cd6cd2237d61abade4c6d48d7a5809a2c2
Deziree Teague <dez.teague@gmail.com>
2018-10-24 16:48:38 -0700

    added question 6 to loop prompt 4 times

--- guessinggame_lab03.js
+++ guessinggame_lab03.js
@@ -1,6 +1,6 @@
-"use strict";
+'use strict';
 
-var userName = prompt('Hello, what is your name?');
+var userName = prompt('Hello, what is your name?').toUpperCase();
 alert('Thanks for playing the Deziree Guessing Game, ' + userName + '!'); //Thanks for playing the Deziree Guessing Game, ___!
 console.log('userName', userName);
 
@@ -11,7 +11,8 @@ alert(userName + ', please respond to the following question with y/yes or n/no.
 var responseOne = prompt('Was I born in Seattle?').toLowerCase();
 if (responseOne === 'y' || responseOne === 'yes') {
   alert('Yes, that is correct!');
-} else { (responseOne === 'n' || responseOne === 'no') 
+} else {
+  (responseOne === 'n' || responseOne === 'no');
   alert('Sorry, that is incorrect. I was born in North Seattle');
 }
 console.log('responseOne', responseOne);
@@ -20,16 +21,18 @@ console.log('responseOne', responseOne);
 var responseTwo = prompt('Did I study Communication at the University of Washington?').toLowerCase();
 if (responseTwo === 'y' || responseTwo === 'yes') {
   alert('Congrats, you are right.');
-} else { (responseTwo === 'n' || responseTwo === 'no') 
+} else {
+  (responseTwo === 'n' || responseTwo === 'no');
   alert('Sorry, wrong answer. I studied Communication and Diversity at the University of Washington.');
 }
 console.log('responseTwo', responseTwo);
 
-//#3 Was my first study abroad trip to Paris? no
-var responseThree = prompt('Was my first study abroad trip to Paris?').toLowerCase();
+//#3 Did I study abroad in Paris? no
+var responseThree = prompt('Did I study abroad in Paris?').toLowerCase();
 if (responseThree === 'n' || responseThree === 'no') {
   alert('Congrats, ' + userName + '! You know me very well.');
-} else { (responseThree === 'y' || responseThree === 'yes') 
+} else {
+  (responseThree === 'y' || responseThree === 'yes');
   alert('Sorry, wrong answer. My first study abroad trip was to Buenos Aires, Argentina.');
 }
 console.log('responseThree', responseThree);
@@ -38,7 +41,8 @@ console.log('responseThree', responseThree);
 var responseFour = prompt('Am I a certified ESL teacher?').toLowerCase();
 if (responseFour === 'y' || responseFour === 'yes') {
   alert('Yes, that is correct.');
-} else { (responseFour === 'n' || responseFour === 'no') 
+} else {
+  (responseFour === 'n' || responseFour === 'no');
   alert('Sorry, that is incorrect. I received my TEFL certification in 2013.');
 }
 console.log('responseFour', responseFour);
@@ -47,7 +51,37 @@ console.log('responseFour', responseFour);
 var responseFive = prompt('Do I aspire to be a full-stack developer?').toLowerCase();
 if (responseFive === 'y' || responseFive === 'yes') {
   alert('Yes, that is correct.');
-} else { (responseFive === 'n' || responseFive === 'no') 
+} else {
+  (responseFive === 'n' || responseFive === 'no');
   alert('Sorry, that is incorrect. I would love to become a full-stack developer');
 }
 console.log('responseFive', responseFive);
+
+//#6 Guess the number of countries I have visited
+//  CONDITION:
+var numberCountriescorrect = 36;
+console.log('numberCountriescorrect', numberCountriescorrect);
+var numberCountriesguess = parseInt(prompt('Guess the number of countries I have visited.  Hint: The answer is between 1-50'));
+console.log('numberCountriesguess', numberCountriesguess);
+
+//   IF TOO HIGH:
+var i = 0;
+while (numberCountriesguess !== 36 && i < 3) {
+  if (numberCountriesguess > 36) {
+    numberCountriesguess = parseInt(prompt('Your guess was too high. Try again'));
+    i++;
+    console.log('i', i);
+  }
+  //  IF TOO LOW:
+  if (numberCountriesguess < 36 && i < 3) {
+    numberCountriesguess = parseInt(prompt('Your guess was too low. Try again'));
+    i++;
+    console.log('i', i);
+  }
+}
+// IF CORRECT:
+if (numberCountriesguess === 36) {
+  alert('Wow, you got it right! I have been to ' + numberCountriescorrect + ' countries.');
+} else {
+  alert('Oh no, you\'re out of tries! The correct answer is 36');
+}
