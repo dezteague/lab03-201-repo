b42ac885e8b78f1712f6cf4ff68d91ffa5f2b95f
Kent Ketter <kent.ketter1@Gmail.com>
2018-10-25 15:12:21 -0700

    Last commit

--- guessinggame_lab03.js
+++ guessinggame_lab03.js
@@ -9,8 +9,8 @@ alert(userName + ', please respond to the following question with y/yes or n/no.
 
 var correctAnswers = 0;
 
-//#1 Was I born in Seattle? yes
-function yesNo() {
+//#1 Was I born in Seattle? yes some code is missing from here
+function yesNo1() {
   var responseOne = prompt('Was I born in Seattle?').toLowerCase();
   if (responseOne === 'y' || responseOne === 'yes') {
     alert('Yes, that is correct!');
@@ -18,12 +18,13 @@ function yesNo() {
   } else {
     (responseOne === 'n' || responseOne === 'no');
     alert('Sorry, that is incorrect. I was born in North Seattle');
+    console.log('responseOne', responseOne);
   }
 }
-console.log('responseOne', responseOne);
+yesNo1();
 
 //#2 Did I study Communication at the University of Washington? yes
-function yesNo() {
+function yesNo2() {
   var responseTwo = prompt('Did I study Communication at the University of Washington?').toLowerCase();
   if (responseTwo === 'y' || responseTwo === 'yes') {
     alert('Congrats, you are right.');
@@ -32,11 +33,12 @@ function yesNo() {
     (responseTwo === 'n' || responseTwo === 'no');
     alert('Sorry, wrong answer. I studied Communication and Diversity at the University of Washington.');
   }
+  console.log('responseTwo', responseTwo);
 }
-console.log('responseTwo', responseTwo);
+yesNo2();
 
 //#3 Did I study abroad in Paris? no
-function yesNo() {
+function yesNo3() {
   var responseThree = prompt('Did I study abroad in Paris?').toLowerCase();
   if (responseThree === 'n' || responseThree === 'no') {
     alert('Congrats, ' + userName + '! You know me very well.');
@@ -45,11 +47,13 @@ function yesNo() {
     (responseThree === 'y' || responseThree === 'yes');
     alert('Sorry, wrong answer. My first study abroad trip was to Buenos Aires, Argentina.');
   }
+  console.log('responseThree', responseThree);
+
 }
-console.log('responseThree', responseThree);
+yesNo3();
 
 //#4 Am I a certified ESL teacher? yes
-function yesNo() {
+function yesNo4() {
   var responseFour = prompt('Am I a certified ESL teacher?').toLowerCase();
   if (responseFour === 'y' || responseFour === 'yes') {
     alert('Yes, that is correct.');
@@ -58,11 +62,13 @@ function yesNo() {
     (responseFour === 'n' || responseFour === 'no');
     alert('Sorry, that is incorrect. I received my TEFL certification in 2013.');
   }
+  console.log('responseFour', responseFour);
 }
-console.log('responseFour', responseFour);
+yesNo4();
+
 
 //#5 Do I aspire to be a full-stack developer? yes
-function yesNo() {
+function yesNo5() {
   var responseFive = prompt('Do I aspire to be a full-stack developer?').toLowerCase();
   if (responseFive === 'y' || responseFive === 'yes') {
     alert('Yes, that is correct.');
@@ -71,12 +77,13 @@ function yesNo() {
     (responseFive === 'n' || responseFive === 'no');
     alert('Sorry, that is incorrect. I would love to become a full-stack developer');
   }
+
 }
-console.log('responseFive', responseFive);
+yesNo5();
 
 //#6 Guess the number of countries I have visited
 //  CONDITION:
-function yesNo() {
+function yesNo6() {
   var numberCountriescorrect = 36;
   console.log('numberCountriescorrect', numberCountriescorrect);
   var numberCountriesguess = parseInt(prompt('Guess the number of countries I have visited.  Hint: The answer is between 1-50'));
@@ -105,9 +112,9 @@ function yesNo() {
     alert('Oh no, you\'re out of tries! The correct answer is 36');
   }
 }
-
+yesNo6();
 //#7 Can you guess one of my favorite outdoor activities?
-function yesNo() {
+function yesNo7() {
   var activities = ['scuba diving', 'hiking', 'paddle boarding', 'kayaking', 'white-water rafting'];
   var activityResponse = prompt('Can you guess one of my favorite outdoor activities?  You will have 6 chances! Good luck :)').toLowerCase();
   var correct = false;
@@ -125,6 +132,7 @@ function yesNo() {
     alert('Miss');
   }
 }
+yesNo7();
 
 //FINAL MESSAGE
 alert('Hey ' + userName + ', you got' + correctAnswers + ' out of 7 questions correct.  Thanks for visiting my site!');
